530d876127059a2c970a744930581efed5623a7c
Phil Sharp <phil.j.sharp@gmail.com>
2020-07-02 18:34:32 +0100

    Fix referencing for implicit aspnetcore assemblies

---
+++ Timetable.Web.Plugin/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Serilog;
+
+namespace Timetable.Web
+{
+    /// <summary>
+    /// Dummy Main method, required due to referencing Microsoft.NET.Sdk.Web
+    /// </summary>
+    /// <remarks>
+    /// Reference Microsoft.NET.Sdk.Web instead of Microsoft.NET.Sdk
+    /// to avoid reference problems required in IPlugin
+    /// </remarks>
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            Log.Fatal("Dummy Main.  Do not run");
+            return 0;
+        }
+    }
+}
