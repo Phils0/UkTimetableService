53fcacb2a51233abb7672955851c212adc210536
Phil Sharp <phil.j.sharp@outlook.com>
2020-09-19 14:24:57 +0100

    Add TocFilter to locations

--- Timetable.Web.Test/Controllers/ReferenceControllerTest.cs
+++ Timetable.Web.Test/Controllers/ReferenceControllerTest.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using NSubstitute;
+using Serilog;
 using Timetable.Test.Data;
 using Timetable.Web.Controllers;
 using Timetable.Web.Mapping;
@@ -19,18 +20,68 @@ namespace Timetable.Web.Test.Controllers
         public async Task ReturnsLocations()
         {
             var data = Substitute.For<ILocationData>();
-            data.Locations.Returns(
-                new Dictionary<string, Station>()
-                {
-                    {"SUR", TestStations.Surbiton},
-                    {"WAT", TestStations.Waterloo}
-                });
+            data.Locations.Returns(TestLocations());
 
-            var controller = new ReferenceController(data, _config.CreateMapper());
+            var controller = new ReferenceController(data, _config.CreateMapper(), Substitute.For<ILogger>());
             var response = await controller.LocationAsync() as ObjectResult;;
             
             Assert.Equal(200, response.StatusCode);
             Assert.NotEmpty(response.Value as Model.Station[]);
+        }
+
+        private static Dictionary<string, Station> TestLocations()
+        {
+            var surbiton = TestStations.Surbiton;
+            AddToc(surbiton, "SW");
+
+            var waterloo = TestStations.Waterloo;
+            AddToc(waterloo, "SW");
+            AddToc(waterloo, "SE");
+            
+            var weybridge = TestStations.Weybridge;
+            AddToc(weybridge, "TL");
+            
+            return new Dictionary<string, Station>()
+            {
+                {"SUR", surbiton},
+                {"WAT", waterloo},
+                {"WYB", weybridge}
+            };
+        }
+
+        private static void AddToc(Station station, string toc)
+        {
+            station.TocServices.Add(new Toc(toc));
+        }
+        
+        
+        public static TheoryData<string[], int> FilterData =>
+            new TheoryData<string[], int>()
+            {
+                {new []{"SW"},  2},
+                {new []{"SE"},  1},
+                {new []{"TL"},  1},
+                {new []{"SW", "SE"},  2},
+                {new []{"TL", "SE"},  2},
+                {new []{"SW", "SE", "TL"},  3},
+                {new []{""},  3},
+                {new string[]{null},  3},
+                {new string[0],  3},
+                {null,  3},
+            };
+        
+        [Theory]
+        [MemberData(nameof(FilterData))]
+        public async Task ReturnsFilteredLocations(string[] tocs, int expected)
+        {
+            var data = Substitute.For<ILocationData>();
+            data.Locations.Returns(TestLocations());
+
+            var controller = new ReferenceController(data, _config.CreateMapper(), Substitute.For<ILogger>());
+            var response = await controller.LocationAsync(tocs) as ObjectResult;
+            
+            var stations = response.Value as Model.Station[];
+            Assert.Equal(expected, stations.Length);
         }
     }
 }
--- Timetable.Web/Controllers/ReferenceController.cs
+++ Timetable.Web/Controllers/ReferenceController.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Serilog;
 
 namespace Timetable.Web.Controllers
 {
@@ -17,25 +19,49 @@ namespace Timetable.Web.Controllers
     {
         private readonly ILocationData _data;
         private readonly IMapper _mapper;
+        private readonly ILogger _logger;
 
-        public ReferenceController(ILocationData data, IMapper mapper)
+        public ReferenceController(ILocationData data, IMapper mapper, ILogger logger)
         {
             _data = data;
             _mapper = mapper;
+            _logger = logger;
         }
 
         /// <summary>
         /// Returns UK rail locations that you can use in this API
         /// </summary>
+        /// <param name="toc">Only services from included TOCs included.  Can add multiple to querystring, then any service ran by any of them returned</param>
         /// <returns>Set of locations</returns>
         /// <response code="200">Ok</response>
+        /// <response code="404">Not Found</response>
+        /// <response code="500">Internal Server error</response>
         [ProducesResponseType(200, Type = typeof(Model.Station[]))]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
         [Route("location")]
         [HttpGet]
-        public async Task<IActionResult> LocationAsync()
+        public async Task<IActionResult> LocationAsync([FromQuery] string[] toc = null)
         {
-            var model = _mapper.Map<IEnumerable<Timetable.Station>, Model.Station[]>(_data.Locations.Values);
-            return await Task.FromResult(Ok(model));
+            try
+            {
+                var tocFilter = new TocFilter(toc);
+
+                var locations = tocFilter.NoFilter ? 
+                    _data.Locations.Values : 
+                    _data.Locations.Values.Where(s => s.TocServices.Any(t => tocFilter.IsValid(t)));
+                var model = _mapper.Map<IEnumerable<Timetable.Station>, Model.Station[]>(locations);
+                if (model.Any())
+                    return await Task.FromResult(Ok(model));
+                
+                return await Task.FromResult(NotFound());
+            }
+            catch (Exception e)
+            {
+                _logger.Error(e, "Error getting locations");
+                return await Task.FromResult(StatusCode(500));
+            }
+
         }
     }
 }
